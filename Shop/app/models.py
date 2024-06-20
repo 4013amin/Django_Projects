926c778e61e26e62f98fd81516ee20a296ea0ac4
delta <amin138400138400@gmail.com>
2024-06-20 13:13:51 +0330

    Testi

--- Shop/app/models.py
+++ Shop/app/models.py
@@ -6,7 +6,7 @@ class Users(models.Model):
     name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     product_name = models.CharField(max_length=30)
-    price = models.CharField(max_length=30)  # Changed max_length to 30
+    price = models.CharField(max_length=30)
     date = models.CharField(max_length=20)
     description = models.CharField(max_length=1500)
 
