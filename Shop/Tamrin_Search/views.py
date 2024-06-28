b5189f5427959486dcf4849847d48c3c0f22bac6
delta <amin138400138400@gmail.com>
2024-06-28 18:56:00 +0330

    Create_form_page

--- Shop/Tamrin_Search/views.py
+++ Shop/Tamrin_Search/views.py
@@ -4,15 +4,19 @@ from . import models
 from .forms import Search_form
 
 
-# Create your views here.
 def Home_Data(request):
     search = Search_form(request.GET)
     if search.is_valid():
         search_results = search.cleaned_data["text_filed"]
         data_search = models.Data.objects.filter(username__icontains=search_results)
 
+        if not data_search.exists():
+            message = "دیتا ها پیدا نشد!"
+        else:
+            message = ""
     else:
         data_search = models.Data.objects.all()
+        message = ""
 
-    context = {'data': data_search, 'search': search}
+    context = {'data': data_search, 'search': search, 'message': message}
     return render(request, 'index_data.html', context)
