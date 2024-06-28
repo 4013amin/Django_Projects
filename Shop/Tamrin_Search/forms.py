0634f56d2551ac97ea1fe2850c85d97497e564ec
delta <amin138400138400@gmail.com>
2024-06-28 18:49:24 +0330

    Create_form_page

---
+++ Shop/Tamrin_Search/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class Search_form(forms.Form):
+    text_filed = forms.CharField(max_length=30, required=False)
--- Shop/Tamrin_Search/views.py
+++ Shop/Tamrin_Search/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 from . import models
+from .forms import Search_form
 
 
 # Create your views here.
 def Home_Data(request):
-    data = models.Data.objects.all()
-    context = {'data': data}
+    search = Search_form(request.GET)
+    if search.is_valid():
+        search_results = search.cleaned_data["text_filed"]
+        data_search = models.Data.objects.filter(username__icontains=search_results)
+
+    else:
+        data_search = models.Data.objects.all()
+
+    context = {'data': data_search, 'search': search}
     return render(request, 'index_data.html', context)
