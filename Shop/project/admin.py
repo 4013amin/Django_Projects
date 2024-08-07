af25ff761a96b32c8518800e562ae38170c22e75
delta <amin138400138400@gmail.com>
2024-08-07 18:17:15 +0330

    create newApp

--- Shop/project/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import users
-
-
-# Register your models here.
-@admin.register(users)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'phone']
--- Shop/project/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class users(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    phone = models.CharField(max_length=30)
