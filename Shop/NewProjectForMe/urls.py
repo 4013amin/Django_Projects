57b813c06f38a5d2e05ac66bd0d156408ef66682
delta <amin138400138400@gmail.com>
2024-10-12 16:16:19 +0330

    Tamrin in NewPorjectForMe

--- Shop/NewProjectForMe/urls.py
+++ Shop/NewProjectForMe/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import sendData
+from .views import sendData, getData
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('sendData/', sendData, name='index'),
-]
+                  path('sendData/', sendData, name='index'),
+                  path('getData', getData)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Shop/NewProjectForMe/views.py
+++ Shop/NewProjectForMe/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializer import DataSerializer
+from . import models
 
 
 @api_view(['POST'])
@@ -14,3 +15,10 @@ def sendData(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def getData(request):
+    data = models.Data.objects.all()
+    serializer = DataSerializer(data, many=True)
+    return Response(serializer.data)
