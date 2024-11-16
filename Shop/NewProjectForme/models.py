9da0f1b20efc5cc782b3d21ca21dc3697e9600d5
delta <amin138400138400@gmail.com>
2024-11-16 14:16:26 +0330

    add product models

--- Shop/NewProjectForme/models.py
+++ Shop/NewProjectForme/models.py
@@ -10,3 +10,19 @@ class Users(models.Model):
 
     def __str__(self):
         return self.username
+    
+
+class Products(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=300)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=30)
+    quantity = models.IntegerField()
+    rating = models.FloatField()
+    views = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
