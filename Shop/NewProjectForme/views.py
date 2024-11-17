a6cb42c8ae5c55b4c4d35b9c20252c78c7981e86
delta <amin138400138400@gmail.com>
2024-11-17 11:10:02 +0330

    change

--- Shop/NewProjectForme/views.py
+++ Shop/NewProjectForme/views.py
@@ -35,3 +35,12 @@ class LoginView(APIView):
                 {"error": "Invalid username or password"},
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+class save(APIView):
+    def save(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
