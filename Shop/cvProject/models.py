863444faa4b0b2ee4adeb68f34f14f4f593efe60
Delta <amin138400138400@gmail.com>
2025-03-19 06:25:26 -0700

    delete models in cvProject

--- Shop/cvProject/models.py
+++ Shop/cvProject/models.py
@@ -79,6 +79,3 @@ class Booking(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.concert.title}"
     
-
-class Dunamic_Models(models.Model):
-    user = models.ForeignKey(User , on_delete=models.CASCADE , null= False)
