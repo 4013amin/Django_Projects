eed9590f5d5cdf93072c3bb28aa68ddabf7174e9
Delta <amin138400138400@gmail.com>
2025-07-18 15:13:06 +0330

    Edit change

--- Shop/Shop/settings.py
+++ Shop/Shop/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'app',
     'Tamrin_Search',
     'Tamrin',
@@ -48,8 +47,19 @@ INSTALLED_APPS = [
     'cvProject',
     'Authentication',
     'channels',
-    'API'
+    'API',
+    'rest_framework',
+    'drf_yasg',
+    'rest_framework.authtoken',
+
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Shop/Shop/urls.py
+++ Shop/Shop/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 # Shop/urls.py
 
 
@@ -40,7 +43,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('API.url')),
 
-
+    # مسیرهای مستندات Swagger و Redoc
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
