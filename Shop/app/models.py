1965a52b28103ef1cc5c5376c09a6684804ab633
delta <amin138400138400@gmail.com>
2025-11-20 21:39:03 +0330

    add model in applications app

--- Shop/app/models.py
+++ Shop/app/models.py
@@ -26,3 +26,13 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
+    
